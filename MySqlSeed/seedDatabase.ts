b9841a5156c24ee34141fa84fce330065f3df035
Lukasz17git <lucas.m2295@gmail.com>
2023-07-08 23:56:16 +0200

    .

--- MySqlSeed/seedDatabase.ts
+++ MySqlSeed/seedDatabase.ts
@@ -11,20 +11,16 @@ const seedDatabase = () => {
 
    const users = Array(10).fill(0).map(_ => generateRandomUser())
    users.forEach(user => createUserInDB(user))
-   console.log('users', users)
 
    const reviews = Array(20).fill(0).map(_ => generateRandomReview())
    reviews.forEach(review => createReviewInDB(review))
-   console.log('reviews', reviews)
 
    const rooms = Array(10).fill(0).map(_ => generateRandomRoom())
    rooms.forEach(room => createRoomInDB(room))
-   console.log('rooms', rooms)
 
    const roomIds = rooms.map(room => room.id)
    const bookings = Array(30).fill(0).map(_ => generateRandomBooking(roomIds))
    bookings.forEach(booking => createBookingInDB(booking))
-   console.log('bookings', bookings)
 
 }
 
